06d531be01d1f18470f6d5f2f96c8597966f0fc5
Enes Demirtas <enesdemirtas255@gmail.com>
2022-02-11 02:20:19 +0300

    Session, login, and logout features are added.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User")
+const bcrypt = require("bcrypt")
 
 exports.createUser = async (req, res) => {
     try {
@@ -14,4 +15,33 @@ exports.createUser = async (req, res) => {
             error,
         })
     }
+}
+
+exports.loginUser = (req, res) => {
+    try {
+        const { email, password } = req.body
+
+        User.findOne({ email }, (err, user) => {
+            if (user) {
+                bcrypt.compare(password, user.password, (err, same) => {
+                    if (same) {
+                        // User Session
+                        req.session.userID = user._id
+                        res.status(200).redirect("/")
+                    }
+                })
+            }
+        })
+    } catch (error) {
+        res.status(400).json({
+            status: "fail",
+            error,
+        })
+    }
+}
+
+exports.logoutUser = (req, res) => {
+    req.session.destroy(() => {
+        res.redirect("/")
+    })
 }
